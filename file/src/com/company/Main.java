1d13b4f769fcf950d639c0f21c3667c4fcdb676c
gemary <ducdang1206@gmail.com>
2019-11-30 10:10:36 +0700

    secondCommit

--- file/src/com/company/Main.java
+++ file/src/com/company/Main.java
@@ -5,11 +5,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
        String data = Readfile("/home/trungduc/Desktop/hoten/file/src/com/company/cadao.txt");
-        System.out.println(data);
+       // System.out.println(data);
+        String newData = ReadUTF8("/home/trungduc/Desktop/hoten/file/src/com/company/cadao.txt");
+        System.out.println(newData);
     }
+    public  static  String ReadUTF8(String FileName) throws IOException {
+        String Data ="";
+        Reader reader = new FileReader(FileName);
+        BufferedReader buffer =new BufferedReader(reader);
+        int c,i =1 ;
+        String Line;
 
+        while ((Line = buffer.readLine()) !=null){
+            Data+= i +"-"+ Line + "\n";
+            i++;
+        }
+        return Data;
+    }
     public static String Readfile(String fileName){
         File file = new File(fileName);
         String data ="";
@@ -18,7 +32,7 @@ public class Main {
             int c;
             while ( (c = scanner.read() ) !=-1){
                 data+= (char)c;
-                System.out.println(c);
+                //System.out.println(c);
             }
             scanner.close();
         } catch (FileNotFoundException e) {
