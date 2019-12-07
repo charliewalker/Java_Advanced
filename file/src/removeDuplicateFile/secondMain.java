27f8afcd5f2481bc342587f3cda00c58db651bde
gemary <ducdang1206@gmail.com>
2019-12-07 08:30:03 +0700

    thirdcommit

--- file/src/removeDuplicateFile/secondMain.java
+++ file/src/removeDuplicateFile/secondMain.java
@@ -10,12 +10,11 @@ import java.util.List;
 public class secondMain {
 
     public static  void  main(String [] args) throws IOException {
-        String path ="/home/trungduc/Desktop/hoten/file";
+        String path ="/home/trungduc/Desktop/hoten/test";
        // listFiles(path);
         removeDuplicatedFiles(path,"caodao.txt");
     }
     public static ArrayList<String> listPaths = new ArrayList<>();
-    public static ArrayList<Long> flag= new ArrayList<>();
     public static void listFiles(String folderPath) throws IOException {
 
         File folder = new File(folderPath);
@@ -39,41 +38,46 @@ public class secondMain {
             }
         }
     }
-
-    public static void removeDuplicatedFiles(String folderPath, String filename) throws IOException {
-        File folder = new File(folderPath);
-        File [] listFile =folder.listFiles();
-
-        for(File f: listFile){
-            System.out.println(f.getName());
-            System.out.println(f.length());
+    public  static  int checkDuplicateFIle(String filePath, String filename) throws IOException {
+        File file = new File(filePath);
+        File [] listfile = file.listFiles();
+        for (File f: listfile){
             if (f.isFile()){
-                    if (f.getName().equals(filename)){
-                        System.out.println(filename);
-                       if ( flag.size() <= 0){
-                           flag.add(f.length());
-                       }
-                       else {
-                           if (flag.get(0) == f.length()){
-                               f.delete();
-                               System.out.println("Mission Success!");
-                               System.out.println("Delete at:"+ f.getCanonicalPath());
-                           }
-
-                       }
-
-                    }
-                    else {
-                        System.out.println("Mission fail!");
-                    }
+                if (f.getName().equals(filename)){
+                    listPaths.add(f.getCanonicalPath());
+                }
             }
             else {
-                System.out.println(f.getPath());
-                removeDuplicatedFiles(f.getPath(),filename);
+                checkDuplicateFIle(f.getPath(),filename);
             }
+
         }
+        return listPaths.size();
+    }
+    public static void removeDuplicatedFiles(String folderPath, String filename) throws IOException {
+        int success = checkDuplicateFIle(folderPath, filename);
+        if (success >1){
+           for (int i = 0 ; i < listPaths.size(); i++){
+               File file = new File(listPaths.get(i));
+               System.out.println(file.getName());
+               System.out.println(file.length());
+               for (int j = i+1 ; j < listPaths.size();j++){
+                   File file1 = new File(listPaths.get(j));
+                   System.out.println(file1.getName());
+                   System.out.println(file1.length());
+                   if (file.length() == file1.length()){
+                       file.delete();
+                       System.out.println("Mission success!");
+                       System.out.println(file.getCanonicalPath());
+                   }
+               }
 
+           }
 
+        }
+        else {
+            System.out.println("Dont have Duplicate File!");
+        }
 
     }
 
